6d64b42e649e58dd517bff10b83c180b9f4f2d30
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-07-09 22:24:04 +0700

    added database

--- sianas.sql
+++ sianas.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Bulan Mei 2023 pada 18.59
--- Versi server: 10.4.8-MariaDB
--- Versi PHP: 7.3.11
+-- Generation Time: Jul 09, 2023 at 05:22 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -38,7 +37,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`no_admin`, `nama`, `username`, `password`, `role`, `foto`) VALUES
@@ -48,7 +47,7 @@ INSERT INTO `admin` (`no_admin`, `nama`, `username`, `password`, `role`, `foto`)
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `anggota`
+-- Table structure for table `anggota`
 --
 
 CREATE TABLE `anggota` (
@@ -65,16 +64,16 @@ CREATE TABLE `anggota` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `anggota`
+-- Dumping data for table `anggota`
 --
 
 INSERT INTO `anggota` (`no_anggota`, `subbag`, `nama`, `nip`, `jabatan`, `no_hp`, `username`, `password`, `role`, `foto`) VALUES
-(1, 'Komisi B', 'Andri', '198508172021212015', 'Staff Komisi', '089373429', 'andri', '12345', 2, 'default.png');
+(1, 'Komisi B', 'Andrii', '198508172021212015', 'Staff Komis', '089373429', 'andri', '12345', 2, 'default.png');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `mobil`
+-- Table structure for table `mobil`
 --
 
 CREATE TABLE `mobil` (
@@ -97,30 +96,19 @@ CREATE TABLE `mobil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `mobil`
+-- Dumping data for table `mobil`
 --
 
 INSERT INTO `mobil` (`no_mobil`, `nama`, `no_hp`, `alamat`, `username`, `password`, `role`, `jenis_mobil`, `no_plat`, `tgl_pjk`, `no_rangka`, `no_mesin`, `no_stnk`, `warna`, `status`, `foto`) VALUES
-(17, 'Yanto', '097675', 'Jl. Bringin Raya no 128, Ungaran', 'yanto', '12345', 3, 'Toyota HAICE ', 'H 9517 AG', '2022-07-21', '4GHJM34JHNB209284', 'VR001FMG17000001', '08698264', 'abu', 'Digunakan', ''),
+(17, 'Yanto b', '097675', 'Jl. Bringin Raya no 128, Ungaran', 'yanto', '097675', 3, 'Toyota HAICE ', 'H 9517 AG', '2022-07-21', '4GHJM34JHNB209284', 'VR001FMG17000001', '08698264', 'abu', 'Digunakan', ''),
 (18, 'biyu', '09876', 'Jl. Jendral Soedirman no 34, Salatiga', 'biyu', '12345', 3, 'Toyota HAICE', 'H 9515 AG', '2022-12-21', 'FGHN43JHNM324235', 'MN008JKA37000004', '09384595', 'abu', 'Tidak_digunakan', ''),
 (19, 'Andi', '08969097', 'Jl. Ahmad Yani no 67, Solo', 'andisopir', 'andisopir', 3, 'Toyota HAICE', 'H 9518 AG', '2022-07-21', '2HJMJ98VGHN987424', 'KL003KAL89000001', '09374809', 'abu', 'Tidak_digunakan', ''),
-(20, '', '', '', '', '', 0, 'Toyota HIACE', 'H 9516 AG', '2022-07-21', '8VBNM82YUIK924563', 'SD001MND3100003', '37408274', 'abu', 'Digunakan', ''),
-(21, '', '', '', '', '', 0, 'Toyota HAICE', 'H 9515 AG', '2022-07-21', '6DFGH85HBVJ567369', 'KL009HDK35000002', '37490284', 'hitam', 'Digunakan', ''),
-(22, '', '', '', '', '', 0, 'Toyota Innova', 'H 1213 XG', '2022-05-15', '7FGHK43CVBN988424', 'JK001JDN12000001', '45028495', 'hitam', 'Digunakan', ''),
-(23, '', '', '', '', '', 0, 'Toyota Innova', 'H 1229 AG', '2022-05-15', '4VBNK89CVBF098763', 'LS003HDK23000003', '85994574', 'hitam', 'Digunakan', ''),
-(24, '', '', '', '', '', 0, 'Toyota Innova', 'H 1225 AG', '2022-05-15', '9HJKM09VGHJ865534', 'JF002JSD43000004', '75994583', 'hitam', 'Digunakan', ''),
-(25, '', '', '', '', '', 0, 'Toyota Innova', 'H 1220 XG', '2022-05-15', '4MNDA34KDUN126473', 'KB009HSL12000004', '93794798', 'hitam', 'Digunakan', ''),
-(26, '', '', '', '', '', 0, 'Toyota Innova', 'H 1218 XG', '2022-05-15', '0JEND11ARAR152201', 'MN004GSK18000005', '57375890', 'hitam', 'Digunakan', ''),
-(28, '', '', '', '', '', 0, 'Kijang Inova', 'H 9513 CZ', '2022-12-16', '6YKLS50BRJK759204', 'JE004ARS05000003', '47503857', 'hitam', 'Digunakan', ''),
-(29, '', '', '', '', '', 0, 'Kijang Inova', 'H 9512 CZ ', '2022-12-16', '9NJAB53KWER210934', 'MK006BHS45000002', '85904578', 'hitam', 'Digunakan', ''),
-(30, 'Supriyanto', '085782012', 'Jl Supriyadi no 89, Semarang', 'sopir', '12345', 3, 'Kijang Inova', 'H 9511 CZ', '2022-12-17', '1HGBH41JXMN109186', 'SL002NCM69000004', '09573986', 'hitam', 'Tidak_digunakan', 'default.png'),
-(31, '', '', '', '', '', 0, 'Kijang Inova', 'H 9528 BZ', '2022-12-16', '5JKEY98KLOM129452', 'CV009FNL78000003', '09874847', 'hitam', 'Digunakan', 'default.png'),
-(39, 'gendon', '08956533', '', 'gendhon', '12345', 3, 'supra', 'H 089765', '0000-00-00', '', '', '', '', '', '');
+(30, 'Supriyanto', '085782012', 'Jl Supriyadi no 89, Semarang', 'sopir', '12345', 3, 'Kijang Inova', 'H 9511 CZ', '2022-12-17', '1HGBH41JXMN109186', 'SL002NCM69000004', '09573986', 'hitam', 'Tidak_digunakan', 'default.png');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `motor`
+-- Table structure for table `motor`
 --
 
 CREATE TABLE `motor` (
@@ -131,11 +119,10 @@ CREATE TABLE `motor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `motor`
+-- Dumping data for table `motor`
 --
 
 INSERT INTO `motor` (`no_motor`, `jenis_motor`, `no_plat`, `tgl_pjk`) VALUES
-(1, 'Supra', 'H 1568 AB', '2022-11-11'),
 (2, 'Supra', 'H 8319 JE', '2022-09-16'),
 (3, 'Jupiter MX', 'H 0928 KM', '2022-12-12'),
 (7, 'Supra', 'H 4571 AG', '2022-12-22'),
@@ -149,7 +136,7 @@ INSERT INTO `motor` (`no_motor`, `jenis_motor`, `no_plat`, `tgl_pjk`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `perawatan`
+-- Table structure for table `perawatan`
 --
 
 CREATE TABLE `perawatan` (
@@ -162,7 +149,7 @@ CREATE TABLE `perawatan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `perawatan`
+-- Dumping data for table `perawatan`
 --
 
 INSERT INTO `perawatan` (`no_perawatan`, `no_mobil`, `jenis_kendaraan`, `no_plat`, `perawatan`, `surat`) VALUES
@@ -171,7 +158,7 @@ INSERT INTO `perawatan` (`no_perawatan`, `no_mobil`, `jenis_kendaraan`, `no_plat
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `riwayat`
+-- Table structure for table `riwayat`
 --
 
 CREATE TABLE `riwayat` (
@@ -196,23 +183,36 @@ CREATE TABLE `riwayat` (
   `surat` varchar(120) NOT NULL,
   `bukti` varchar(120) DEFAULT NULL,
   `konfirmasi` varchar(20) NOT NULL,
-  `konfirmasi_sopir` varchar(120) NOT NULL
+  `konfirmasi_sopir` varchar(120) NOT NULL,
+  `lat_1` varchar(150) NOT NULL,
+  `lng_1` varchar(150) NOT NULL,
+  `lat_2` varchar(150) NOT NULL,
+  `lng_2` varchar(150) NOT NULL,
+  `lat_3` varchar(150) NOT NULL,
+  `lng_3` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `riwayat`
+-- Dumping data for table `riwayat`
 --
 
-INSERT INTO `riwayat` (`no_pengajuan`, `no_anggota`, `no_mobil`, `tujuan_1`, `tujuan_2`, `tujuan_3`, `alamat_1`, `alamat_2`, `alamat_3`, `kota_1`, `kota_2`, `kota_3`, `muatan`, `tgl_digunakan`, `tgl_kembali`, `tgl_upload`, `km_awal`, `km_akhir`, `surat`, `bukti`, `konfirmasi`, `konfirmasi_sopir`) VALUES
-(26, 1, 30, 'Kec Surakarta Utara', 'Kec. Surakarta Barat', '-', 'Jl. surakarta utara', 'jl. surakarta selatan', '-', 'surakarta', 'surakarta', '-', 5, '2023-02-02', '2023-02-04', '2023-02-19 20:39:00', '100000', '1212121', '230219-A22_2020_02813.pdf', '230219-logo_jateng.png', 'Dikonfirmasi', 'Dikonfirmasi'),
-(28, 1, 30, 'dfgj', '-', '-', 'ghj', '-', '-', 'vbn', '-', '-', 6, '2023-03-23', '2023-03-24', '2023-03-29 21:44:55', '100000', '12121212', '230329-A22_2020_02813_-_PROPOSAL_KKI_13.pdf', '230504-SWAFOTO.jpg', 'Dikonfirmasi', 'Dikonfirmasi'),
-(29, 1, 30, 'semarang', '-', '-', 'jakarta', '-', '-', 'ungaran', '-', '-', 10, '2023-05-04', '2023-05-05', '2023-05-04 15:06:09', '16800', '1000', '230504-KTP.pdf', '230504-SWAFOTO1.jpg', 'Dikonfirmasi', 'Dikonfirmasi'),
-(30, 1, 30, 'dgh', '-', '-', 'dfg', '-', '-', '-', '-', '-', 9, '2023-05-06', '2023-05-08', '2023-05-06 21:42:06', '', '', '230506-KTP_11.pdf', NULL, 'Dikonfirmasi', 'Ditolak');
+INSERT INTO `riwayat` (`no_pengajuan`, `no_anggota`, `no_mobil`, `tujuan_1`, `tujuan_2`, `tujuan_3`, `alamat_1`, `alamat_2`, `alamat_3`, `kota_1`, `kota_2`, `kota_3`, `muatan`, `tgl_digunakan`, `tgl_kembali`, `tgl_upload`, `km_awal`, `km_akhir`, `surat`, `bukti`, `konfirmasi`, `konfirmasi_sopir`, `lat_1`, `lng_1`, `lat_2`, `lng_2`, `lat_3`, `lng_3`) VALUES
+(26, 1, 30, 'Kec Surakarta Utara', 'Kec. Surakarta Barat', '-', 'Jl. surakarta utara', 'jl. surakarta selatan', '-', 'surakarta', 'surakarta', '-', 5, '2023-02-02', '2023-02-04', '2023-02-19 20:39:00', '100000', '1212121', '230219-A22_2020_02813.pdf', '230219-logo_jateng.png', 'Dicancel', 'Dikonfirmasi', '', '', '', '', '', ''),
+(28, 1, 30, 'dfgj', '-', '-', 'ghj', '-', '-', 'vbn', '-', '-', 6, '2023-03-23', '2023-03-24', '2023-03-29 21:44:55', '100000', '12121212', '230329-A22_2020_02813_-_PROPOSAL_KKI_13.pdf', '230614-Group_2367.png', 'Dikonfirmasi', 'Dikonfirmasi', '', '', '', '', '', ''),
+(29, 1, 30, 'semarang', '-', '-', 'jakarta', '-', '-', 'ungaran', '-', '-', 10, '2023-05-04', '2023-05-05', '2023-05-04 15:06:09', '16800', '1000', '230504-KTP.pdf', '230504-SWAFOTO1.jpg', 'Ditolak', 'Dikonfirmasi', '', '', '', '', '', ''),
+(30, 1, 30, 'dgh', '-', '-', 'dfg', '-', '-', '-', '-', '-', 9, '2023-05-06', '2023-05-08', '2023-05-06 21:42:06', '', '', '230506-KTP_11.pdf', NULL, 'Ditolak', 'Ditolak', '', '', '', '', '', ''),
+(31, 1, 18, 'jl. adad', 'wdad', 'sdsd', 'sadada', 'dsd', 'sddsd', 'asdadsdd', 'dsdsds', 'dsd', 12, '2023-06-14', '2023-06-30', '2023-06-14 16:37:50', '', '', '230614-report-pdf.pdf', NULL, 'Dicancel', 'Diproses', '', '', '', '', '', ''),
+(32, 1, 18, 'adasd', 'asd', 'dad', 'sdad', 'ads', 'ada', 'ada', 'sad', 'ada', 20, '2023-06-13', '2023-06-29', '2023-06-14 18:03:22', '', '', '230614-thunder-file_020d7d94.pdf', NULL, 'Dicancel', 'Diproses', '', '', '', '', '', ''),
+(33, 1, 19, 'vgg', 'bb', 'vv', 'vv', 'bb', 'bb', 'bb', 'hh', 'hh', 6, '2023-06-14', '2023-06-17', '2023-06-14 19:32:09', '', '', 'Surat_Cuti.pdf', NULL, 'Dicancel', 'Diproses', '', '', '', '', '', ''),
+(34, 1, 30, 'ghh', 'tggy', 'vhhj', 'hbjj', 'bbn', 'gbn', 'ghh', 'ghg', 'ggh', 20, '2023-06-14', '2023-06-14', '2023-06-14 19:42:31', '1120', '11120', 'Laporan_transaksi.pdf', '5.png', 'Dikonfirmasi', 'Dikonfirmasi', '-6.9921283333333335', '110.41679666666666', '-6.992053333333333', '110.41701499999999', '-6.992046666666666', '110.41701666666668'),
+(35, 1, 18, 'yy', '', '', 'ggg', '', '', 'hhh', '', '', 20, '2023-06-14', '2023-06-29', '2023-06-14 19:43:10', '', '', 'surat-melahirkanPGW-2023-00020-05-06-20232.pdf', NULL, 'Ditolak', 'Diproses', '', '', '', '', '', ''),
+(36, 1, 18, 'sss', 's', 's', 's', 's', 's', 's', 's', 's', 0, '2023-06-17', '2023-06-28', '2023-06-17 00:35:47', '', '', '230616-thunder-file_536f6bf3.pdf', NULL, 'Dikonfirmasi', 'Diproses', '', '', '', '', '', ''),
+(37, 1, 18, 's', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 2, '2023-06-17', '2023-06-28', '2023-06-17 00:37:20', '', '', '230616-230219-A22_2020_02813.pdf', NULL, 'Dikonfirmasi', 'Diproses', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `service`
+-- Table structure for table `service`
 --
 
 CREATE TABLE `service` (
@@ -224,7 +224,7 @@ CREATE TABLE `service` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `service`
+-- Dumping data for table `service`
 --
 
 INSERT INTO `service` (`no_service`, `tgl_service`, `no_mobil`, `konfirmasi`, `bukti`) VALUES
@@ -236,7 +236,7 @@ INSERT INTO `service` (`no_service`, `tgl_service`, `no_mobil`, `konfirmasi`, `b
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `superadmin`
+-- Table structure for table `superadmin`
 --
 
 CREATE TABLE `superadmin` (
@@ -249,7 +249,7 @@ CREATE TABLE `superadmin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `superadmin`
+-- Dumping data for table `superadmin`
 --
 
 INSERT INTO `superadmin` (`no_superadmin`, `nama`, `username`, `password`, `role`, `foto`) VALUES
@@ -260,101 +260,101 @@ INSERT INTO `superadmin` (`no_superadmin`, `nama`, `username`, `password`, `role
 --
 
 --
--- Indeks untuk tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`no_admin`);
 
 --
--- Indeks untuk tabel `anggota`
+-- Indexes for table `anggota`
 --
 ALTER TABLE `anggota`
   ADD PRIMARY KEY (`no_anggota`);
 
 --
--- Indeks untuk tabel `mobil`
+-- Indexes for table `mobil`
 --
 ALTER TABLE `mobil`
   ADD PRIMARY KEY (`no_mobil`);
 
 --
--- Indeks untuk tabel `motor`
+-- Indexes for table `motor`
 --
 ALTER TABLE `motor`
   ADD PRIMARY KEY (`no_motor`);
 
 --
--- Indeks untuk tabel `perawatan`
+-- Indexes for table `perawatan`
 --
 ALTER TABLE `perawatan`
   ADD PRIMARY KEY (`no_perawatan`);
 
 --
--- Indeks untuk tabel `riwayat`
+-- Indexes for table `riwayat`
 --
 ALTER TABLE `riwayat`
   ADD PRIMARY KEY (`no_pengajuan`);
 
 --
--- Indeks untuk tabel `service`
+-- Indexes for table `service`
 --
 ALTER TABLE `service`
   ADD PRIMARY KEY (`no_service`);
 
 --
--- Indeks untuk tabel `superadmin`
+-- Indexes for table `superadmin`
 --
 ALTER TABLE `superadmin`
   ADD PRIMARY KEY (`no_superadmin`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `admin`
+-- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `no_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `anggota`
+-- AUTO_INCREMENT for table `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `no_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `no_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `mobil`
+-- AUTO_INCREMENT for table `mobil`
 --
 ALTER TABLE `mobil`
-  MODIFY `no_mobil` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `no_mobil` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
--- AUTO_INCREMENT untuk tabel `motor`
+-- AUTO_INCREMENT for table `motor`
 --
 ALTER TABLE `motor`
   MODIFY `no_motor` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT untuk tabel `perawatan`
+-- AUTO_INCREMENT for table `perawatan`
 --
 ALTER TABLE `perawatan`
   MODIFY `no_perawatan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `riwayat`
+-- AUTO_INCREMENT for table `riwayat`
 --
 ALTER TABLE `riwayat`
-  MODIFY `no_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `no_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
--- AUTO_INCREMENT untuk tabel `service`
+-- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
   MODIFY `no_service` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2025;
 
 --
--- AUTO_INCREMENT untuk tabel `superadmin`
+-- AUTO_INCREMENT for table `superadmin`
 --
 ALTER TABLE `superadmin`
   MODIFY `no_superadmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
